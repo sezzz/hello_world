9c70cd360fa126e8998877998ae3efba7de0c40f
sezzz <sezzz1987@gmail.com>
2016-02-12 15:23:50 +0200

    modified:   hello.py

--- hello.py
+++ hello.py
@@ -7,6 +7,9 @@ def hello_world():
 @app.route('/imgs')
 def img():
     return send_file('imgs/welcome.jpg', mimetype='image/jpg')
+@app.route('/imgs/welcome.jpg')
+def imgs():
+    return send_file('imgs/welcome.jpg', mimetype='image/jpg')
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0')
