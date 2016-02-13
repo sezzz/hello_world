21f5484b11b9a5fa6dad3fa5c90e0a8d60e5f627
sezzz <sezzz1987@gmail.com>
2016-02-14 01:10:24 +0200

    Initial commit.

--- hello.py
+++ hello.py
@@ -1,15 +1,9 @@
-from flask import Flask, send_file
-app = Flask(__name__)
+from flask import Flask
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return 'Hello World!'
-@app.route('/imgs')
-def img():
-    return send_file('imgs/welcome.jpg', mimetype='image/jpg')
-@app.route('/imgs/welcome.jpg')
-def imgs():
-    return send_file('imgs/welcome.jpg', mimetype='image/jpg')
+
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0')
+    application.run(host='0.0.0.0')
